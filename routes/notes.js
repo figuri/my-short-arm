634834ffc242516e4fac56a475b9aeb754ce26c8
unknown <samtisme@gmail.com>
2023-07-31 16:51:43 -0700

    changed router.post so new notes have ID to be called on when deleting a note(may change back later IDK)

--- routes/notes.js
+++ routes/notes.js
@@ -4,26 +4,16 @@ const fs = require("fs");
 // above we are requiring the fs module so we can read and write to the db.json file
 
 // we need to make a get request to read the db.json file and return all saved notes as JSON
-// read the db.json file
-// parse the data
-// return the data to the client
 router.get("/", (req, res) => {
-  res.sendFile("./db/db.json", (err, data) => {
-    // Read the file
+  // Read the file
+  fs.readFile("./db/db.json", (err, data) => {
+    // if there is an error, console log the error and return a 500 status code
     if (err) {
       console.log(err);
       return res.status(500).send("Error reading notes");
-    //   if there is an error reading the file, console log the error and return a 500 status code with a message
     }
-    try {
-        // parse file into an array
-      const notes = JSON.parse(data);
-      res.json(notes);
-    } catch (err) {
-        // if there is an error parsing the data, console log the error and return a 500 status code with a message
-      console.log(err);
-      return res.status(500).send("Error parsing notes");
-    }
+    // if there is no error, parse the data and return it to the client
+    res.json(JSON.parse(data));
   });
 });
 
@@ -39,22 +29,37 @@ router.post("/", (req, res) => {
     const notes = JSON.parse(data);
     // push new note into array
     notes.push(req.body);
-    // write file
+    // write file with id number to reference later
     fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
+        // assing id to note
+        req.body.id = notes.length;
+      // if there is an error, console log the error and return a 500 status code
       if (err) {
         console.log(err);
         return res.status(500).send("Error writing notes");
       }
+      //   if there is no error, return the new note to the client
       res.json(req.body);
     });
   });
 });
+
 // make a route to delete a note based on the id of the note
-router.delete("", (req, res) => {
-  res.sendFile();
+router.delete("/", (req, res) => {
   // Read the file
-  // parse file into an array
-  // filter out the note with the id that was passed in
-  // write file
+  fs.readFile("./db/db.json", (err, data) => {
+    // parse file into an array
+    const notes = JSON.parse(data);
+    // filter out the note with the id that was passed in
+    const filteredNotes = notes.filter((note) => note.id !== req.body.id);
+    // write file
+    fs.writeFile("./db/db.json", JSON.stringify(filteredNotes), (err) => {
+      // if there is an error, console log the error and return a 500 status code
+      if (err) {
+        console.log(err);
+        return res.status(500).send("Error writing notes");
+      }
+    });
+  });
 });
 module.exports = router;
